c76273a358913b57a89255f33bfc92de41a6b885
WieeRd_laptop <wowluke@naver.com>
2021-03-15 21:33:19 +0900

    Game.status() working as intended

--- hardware.py
+++ hardware.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Deals with physical chessboard itself
+Provides interface to LED/Electrode 8x8 matrix
+"""
 import numpy as np
 import gpiozero as gp
 
@@ -11,6 +15,7 @@ class LEDmatrix:
         for row in self.data:
             for col in row:
                 ret += '0' if col else '.'
+                ret += ' '
             ret += '\n'
         return ret
 
@@ -20,7 +25,7 @@ class LEDmatrix:
     def off(self, x:int, y:int):
         self.data[y][x] = False
 
-class Detector:
+class Electrode:
     def __init__(self):
         self.data = np.full((8,8), False)
 
@@ -29,19 +34,25 @@ class Detector:
         for row in self.data:
             for col in row:
                 ret += '@' if col else '.'
+                ret += ' '
             ret += '\n'
         return ret
 
     def scan(self) -> np.ndarray:
-        txt = input('>').split()
-        cmd = txt[0]
-        x, y = int(txt[1]), int(txt[2])
-        if cmd=='lift':
-            self.data[y][x] = False 
-        elif cmd=='place':
-            self.data[y][x] = True
-        return self.data
+        txt = input('>')
+        try:
+            cmd = txt[0]
+            x, y = int(txt[1]), int(txt[2])
+        except:
+            print("WTF")
+        else:
+            if cmd=='l':
+                self.data[y][x] = False 
+            elif cmd=='p':
+                self.data[y][x] = True
+        finally:
+            return self.data
 
 red = LEDmatrix()
 blue = LEDmatrix()
-electrode = Detector()
+detector = Electrode()
--- software.py
+++ software.py
@@ -2,23 +2,38 @@
 import chess
 import numpy as np
 import hardware as hw
+import itertools as it
 from enum import Enum
+from functools import wraps
 
-class Team:
+class Player:
+    """
+    Contains player specific info
+    such as name, picked up pieces
+    """
     def __init__(self, name:str):
         self.name = name
         self.in_air = set()
 
 class Tile(Enum):
+    '''Tile state (Enum type) '''
     EMPTY = 0
     PLACED = 1
     MISSING = 2
     MISPLACED = 3
     SELECT = 4
 
+def event(func):
+    '''debugging purpose; monitor occured events'''
+    @wraps(func)
+    def ret(*args):
+        print(f"Event occured: {func.__name__}{args[1:]}")
+        return func(*args)
+    return ret
+
 class Game:
     def __init__(self):
-        self.team = [Team("White"), Team("Black")]
+        self.player = [Player("White"), Player("Black")]
         self.turn = -1
 
         self.pending = True
@@ -30,5 +45,36 @@ class Game:
         self.board = chess.Board()
         self.tiles = np.full((8,8), Tile.EMPTY)
 
+    def status(self) -> str:
+        sp_brd = str(self.board).split(sep='\n')
+        sp_det = hw.detector.status().split(sep='\n')
+        sp_blu = hw.blue.status().split(sep='\n')
+        sp_red = hw.red.status().split(sep='\n')
+        ret = ''
+        for i in range(8):
+            ret += sp_brd[i] + '  ' + sp_det[i] + '  ' + sp_blu[i] + '  ' + sp_red[i] + '\n'
+        return ret
+
     def play(self):
+        prev = hw.detector.scan().copy()
+        while True:
+            print(self.status())
+            curr = hw.detector.scan()
+            for y, x in it.product(range(8), range(8)):
+                if curr[y][x]!=prev[y][x]:
+                    if curr[y][x]:
+                        self.on_place(x, y)
+                    else:
+                        self.on_lift(x, y)
+            prev = curr.copy()
+
+    @event
+    def on_place(self, x:int, y:int):
         pass
+
+    @event
+    def on_lift(self, x:int, y:int):
+        pass
+
+match = Game()
+match.play()
