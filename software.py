51b2768928c43ba44f4493504c71f27cd4653d55
WieeRd_laptop <wowluke@naver.com>
2021-03-19 21:03:10 +0900

    sub-event: select/unselect

--- software.py
+++ software.py
@@ -8,15 +8,6 @@ from enum import Enum
 from functools import wraps
 from typing import Optional, Set, Tuple
 
-class Player:
-    """
-    Contains player specific info
-    such as name, picked up pieces
-    """
-    def __init__(self, name:str):
-        self.name = name
-        self.in_air: Set[Tuple[int, int]] = set()
-
 class Tile(Enum):
     "Tile state (Enum)"
     EMPTY = '.'
@@ -35,11 +26,12 @@ def event(func):
 
 class Game:
     def __init__(self):
-        self.players = [Player('Black'), Player('White')]
-        self.turn = 0       # game starts at 1(White) not 0(Black)
-        self.pending = True # since pending is default set to True 
+        # color = turn%2; turn = (turn+1)%2
+        self.turn = 1 # 2 1 0 1 0 (white=1, black=0)
+        self.pending = True
         self.errors = 0
 
+        self.in_air = [set(), set()]
         self.select = None # position of selected piece
         self.ps_moves = () # possible moves for selected piece
 
@@ -65,60 +57,86 @@ class Game:
         sp_til = self.TileStatus().split(sep='\n')
         ret = ''
         for i in range(8):
-            ret += sp_det[7-i] + ' ' + sp_brd[i] + '  ' + sp_til[7-i] + ' ' + sp_led[7-i]
+            ret += str(8-i) + ' '
+            ret += sp_brd[i] + '  ' + sp_det[7-i] + ' ' + sp_til[7-i] + ' ' + sp_led[7-i]
             ret += '\n'
+        ret += '  a b c d e f g h\n'
         return ret
 
+    def color_at(self, x, y) -> chess.Color:
+        # piece_at could return None, so have fun with AttributeError
+        return self.board.piece_at(chess.square(x, y)).color
+
     def play(self):
-        prev = hw.detector.scan().copy()
+        # prev = hw.detector.scan().copy()
+        prev = hw.detector.data.copy()
         while True:
             print(self.status())
             curr = hw.detector.scan()
             for y, x in it.product(range(8), range(8)):
                 if curr[y][x]!=prev[y][x]:
-                    if curr[y][x]:
-                        self.on_place(x, y)
-                    else:
+                    if prev[y][x]:
                         self.on_lift(x, y)
+                    else:
+                        self.on_place(x, y)
             prev = curr.copy()
-
-    @event
-    def on_place(self, x:int, y:int):
-        tile = self.tiles[y][x]
-        if   tile==Tile.EMPTY:
-            if (x, y) in self.ps_moves:
-                self.on_move(x, y)
-            else:
-                self.on_misplace(x, y)
-        elif tile==Tile.MISSING:
-            if (x, y) in self.ps_moves:
-                self.on_kill(x, y)
-            else:
-                self.on_retrieve(x, y)
-        elif tile==Tile.SELECT:
-            self.on_unselect(x, y)
 
     @event
     def on_lift(self, x:int, y:int):
         tile = self.tiles[y][x]
-        if   tile==Tile.GROUND:
-            piece = self.board.piece_at(chess.square(x, y))
-            owner = self.players[piece.color]
-            owner.in_air.add((x, y))
-            if self.turn==piece.color and len(owner.in_air)==1:
+        if tile not in (Tile.GROUND, Tile.WRONG):
+            print(f"WTF: on_lift at {tile}")
+            exit(-1)
+        elif tile==Tile.GROUND:
+            color = self.color_at(x, y)
+            self.in_air[color].add((x, y))
+            if self.turn==color and len(self.in_air[color])==1:
                 self.on_select(x, y)
             else:
                 self.on_missing(x, y)
         elif tile==Tile.WRONG:
             self.on_cleanup(x, y)
-        else:
-            print(f"WTF: on_lift occured at {tile}")
-            exit(-1)
 
     @event
-    def on_select(self, x, y): pass
+    def on_place(self, x:int, y:int):
+        tile = self.tiles[y][x]
+        if tile not in (Tile.EMPTY, Tile.MISSING, Tile.SELECT):
+            print(f"WTF: on_place at {tile}")
+            exit(-1)
+        elif tile==Tile.EMPTY:
+            if (x, y) in self.ps_moves:
+                self.on_move(x, y)
+            else:
+                self.on_misplace(x, y)
+        else: # can know the owner of piece
+            color = self.color_at(x, y)
+            self.in_air[color].remove((x, y))
+            if tile==Tile.MISSING:
+                if (x, y) in self.ps_moves:
+                    self.on_kill(x, y)
+                else:
+                    self.on_retrieve(x, y)
+            elif tile==Tile.SELECT:
+                self.on_unselect(x, y)
+
     @event
-    def on_unselect(self, x, y): pass
+    def on_select(self, x, y):
+        self.tiles[y][x] = Tile.SELECT
+        self.select = (x, y)
+        func = lambda m: m.from_square==chess.square(x, y)
+        tmp = filter(func, self.board.legal_moves)
+        tmp = (divmod(x.to_square, 8) for x in tmp)
+        self.ps_moves = tuple((x, y) for y, x in tmp)
+        for x, y in self.ps_moves:
+            hw.blue.on(x, y)
+
+    @event
+    def on_unselect(self, x, y):
+        self.tiles[y][x] = Tile.GROUND
+        for x, y in self.ps_moves:
+            hw.blue.off(x, y)
+        self.select = None
+        self.ps_moves = ()
 
     @event
     def on_missing(self, x, y): pass
