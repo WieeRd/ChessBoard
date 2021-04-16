c396d35c94f43539141e7e53e65cee2e1036768b
RPI3B+ Server <wowluke@naver.com>
2021-04-16 15:16:42 +0900

    test.py: Implement matrix control

--- test.py
+++ test.py
@@ -1,25 +1,69 @@
+from abc import ABCMeta, abstractmethod
+
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial)
+# serial = spi(port=0, device=0, gpio=noop())
+# device = max7219(serial)
 
-with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="white", fill="black")
+# with canvas(device) as draw:
+#         draw.rectangle(device.bounding_box, outline="white", fill="black")
 
-import numpy as np
+class LEDmatrix(metaclass=ABCMeta):
+    """
+    Abstract base class for controlling LED matrix
+    """
+    @abstractmethod
+    def on(self, x:int, y:int): pass
 
-class MatrixChain:
-    def __init__(self, serial: spi, chain: int = 1):
-        self.chain = chain
-        self.device = max7219(serial, cascaded=chain)
-        self.data = np.full((8*chain, 8), True)
+    @abstractmethod
+    def off(self, x:int, y:int): pass
+
+    @abstractmethod
+    def toggle(self, x:int, y:int): pass
+
+    @abstractmethod
+    def flush(self): pass
+
+class DummyMatrix(LEDmatrix):
+    """
+    Virtual device that doesn't do anything
+    other than just storing changes made
+    """
+    def __init__(self):
+        self.data = [[False]*8]*8
+
+    def on(self, x:int, y:int):
+        self.data[y][x] = True
+
+    def off(self, x:int, y:int):
+        self.data[y][x] = False
+
+    def toggle(self, x:int, y:int):
+        self.data[y][x] = not self.data[y][x]
+
+    def flush(self):
+        pass
+
+class MatrixChain(LEDmatrix):
+    """
+    Controls multiple daisy-chained max7219 LED matrix
+    Change data attribute via on(), off(), toggle()
+    and apply it with flush()
+    """
+    def __init__(self, serial: spi, cascaded: int = 1):
+        self.cascaded = cascaded
+        self.device = max7219(serial, cascaded=cascaded)
+
+        self.height = 8
+        self.width = 8*cascaded
+        self.data = [[False]*self.width]*self.height
 
     def flush(self):
         with canvas(self.device) as draw:
-            for y in range(8):
-                for x in range(8*self.chain):
+            for y in range(self.height):
+                for x in range(self.width):
                     if self.data[y][x]:
                         draw.point((x, y), fill="white")
 
@@ -32,6 +76,22 @@ class MatrixChain:
     def toggle(self, x:int, y:int):
         self.data[y][x] = not self.data[y][x]
 
-class SingleMatrix:
+class SingleMatrix(LEDmatrix):
+    """
+    Control single LED matrix among daisy-chained devices
+    """
     def __init__(self, chain: MatrixChain, offset: int):
-        pass
+        self.chain = chain
+        self.offset = offset*8
+
+    def on(self, x:int, y:int):
+        self.chain.on(x+self.offset, y)
+
+    def off(self, x:int, y:int):
+        self.chain.off(x+self.offset, y)
+
+    def toggle(self, x:int, y:int):
+        self.chain.toggle(x+self.offset, y)
+
+    def flush(self):
+        self.chain.flush()
